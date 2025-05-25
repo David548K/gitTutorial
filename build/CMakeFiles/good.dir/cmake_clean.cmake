80e982fefb1fa4f07de65e3b57dc1dca3c8e7a25
David548K <angeldavid548K@outlook.com>
2025-05-24 22:40:28 -0300

    Version 1

---
+++ build/CMakeFiles/good.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/good.dir/main.cpp.obj"
+  "CMakeFiles/good.dir/main.cpp.obj.d"
+  "good.exe"
+  "good.exe.manifest"
+  "good.pdb"
+  "libgood.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/good.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(){
+
+    std::cout << "Version 1.0" << std::endl;
+
+    return 0;
+}
